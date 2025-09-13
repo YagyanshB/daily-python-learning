b2ffc142185a70ee6427423795ff320883d43571
Yagyansh Bagri <ybagri@hotmail.com>
2025-09-13 13:51:39 +0100

    upload

---
+++ string_manipulation.py
@@ -0,0 +1,14 @@
+print("Welcome to the Band Name Generator Program")
+
+# asking the users for an input
+
+first_name = input("What is your favorite name? ")
+second_name = input("What is your favorite color? ")
+
+print(f"The Band Name is: {first_name} {second_name}")
+
+
+
+
+
+
